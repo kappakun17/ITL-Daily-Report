6747f11f22e05511644c86f74606df1060611258
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-16 15:03:25 +0900

    プロジェクト ファイルを追加します。

---
+++ ITL-Daily-Report/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\source\repos\ITL-Daily-Report\ITL-Daily-Report\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
