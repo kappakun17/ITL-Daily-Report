c168e36ef948de52792856f225b64ce628938832
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-16 19:07:17 +0900

    ユーザーとスタッフのサインイン画面（登録したアカウント名が反映されるか）を実装しました

--- ITL-Daily-Report/user/views.py
+++ ITL-Daily-Report/user/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from user.models import User, Staff
 
 # Create your views here.
 
 def userSignIn(request):
-    context = {'test':'welcome to Sign In'}
+    data = User.objects.get(id=1)
+    context = {'test': data}
+    return render(request, 'user/base.html',context)
+
+def staffSignIn(request):
+    data = Staff.objects.get(id=1)
+    context = {'test': data}
     return render(request, 'user/base.html',context)
