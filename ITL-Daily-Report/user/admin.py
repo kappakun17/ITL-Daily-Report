5310078e7f944852edcabb440e87631faf516e55
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-16 19:03:56 +0900

    管理者サイトにUser, Staffモデルを追加しました。

--- ITL-Daily-Report/user/admin.py
+++ ITL-Daily-Report/user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from user.models import User, Staff
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name', 'email', 'is_active')
+    #list_editable = ('is_active')
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name','last_name', 'email', 'is_active', 'is_staff', 'is_traineer')
+    #list_editable = ('is_active', 'is_staff', 'is_traineer')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Staff, StaffAdmin)
+
