bda2725425f892c5ab9d072eeba2300487e5541a
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-16 19:06:15 +0900

    スタッフのサインインパスを追加しました。

--- ITL-Daily-Report/user/urls.py
+++ ITL-Daily-Report/user/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='user'
 
-urlpatterns = {
-    path('sign-in', views.userSignIn, name='userSignIn')}
+urlpatterns = [
+    path('sign-in', views.userSignIn, name='userSignIn'),
+    path('staff/sign-in', views.staffSignIn, name='staffSignIn')]
