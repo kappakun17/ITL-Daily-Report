4f85ef1c1f8f79d4a87fdf6c415ac27372345ac7
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-17 16:49:01 +0900

    `User Managerを作りました。

--- ITL-Daily-Report/user/models.py
+++ ITL-Daily-Report/user/models.py
@@ -4,6 +4,14 @@ from django.utils import timezone
 # Create your models here.
 
 
+class UserManager(models.Manager):
+   def get_or_none(self, **kwargs):
+       
+       try:
+           return self.get_queryset().get(**kwargs)
+       except self.model.DoesNotExist:
+           return None
+
 class User(models.Model):
     first_name = models.CharField('first name', max_length=100)
     last_name = models.CharField('last name', max_length=100)
@@ -11,6 +19,8 @@ class User(models.Model):
     password = models.CharField('password', max_length=50)
     is_active = models.BooleanField('is_active', default=False)
     date_joined = models.DateTimeField('date time', default = timezone.now)
+
+    manager = UserManager()
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
