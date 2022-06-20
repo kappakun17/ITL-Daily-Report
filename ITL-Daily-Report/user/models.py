1c8686f0310101b02c2a2ad259483d11994e63a9
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-20 18:18:46 +0900

    add staff manager for get the data match the key

--- ITL-Daily-Report/user/models.py
+++ ITL-Daily-Report/user/models.py
@@ -12,11 +12,19 @@ class UserManager(models.Manager):
        except self.model.DoesNotExist:
            return None
 
+class StaffManager(models.Manager):
+    def get_or_none(self, **kwargs):
+       
+       try:
+           return self.get_queryset().get(**kwargs)
+       except self.model.DoesNotExist:
+           return None
+
 class User(models.Model):
     first_name = models.CharField('first name', max_length=100)
     last_name = models.CharField('last name', max_length=100)
     email = models.EmailField('email',max_length=250)
-    password = models.CharField('password', max_length=50)
+    password = models.CharField('password', max_length=150)
     is_active = models.BooleanField('is_active', default=False)
     date_joined = models.DateTimeField('date time', default = timezone.now)
 
@@ -36,11 +44,12 @@ class Staff(models.Model):
     first_name = models.CharField('first name', max_length=100)
     last_name = models.CharField('last name', max_length=100)
     email = models.EmailField('email',max_length=250)
-    password = models.CharField('password', max_length=50)
-    is_staff = models.BooleanField('is_staff', default = True)
+    password = models.CharField('password', max_length=150)
     is_active = models.BooleanField('is_active', default=False)
     is_traineer = models.BooleanField('is_traineer', default=False)
     date_joined = models.DateTimeField('date time', default = timezone.now)
+
+    manager= StaffManager()
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
