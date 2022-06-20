954635e94039f9f409876d8aee6fef279b061b36
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-20 18:12:03 +0900

    fix first name and last name's field

--- ITL-Daily-Report/user/forms.py
+++ ITL-Daily-Report/user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import User, Staff
 
 
 class SignInForm(forms.Form):
@@ -7,8 +8,8 @@ class SignInForm(forms.Form):
     
 
 class SignUpForm(forms.Form):
-    last_name = forms.CharField(label='姓', max_length=100)
-    first_name = forms.CharField(label='名', max_length=100)
+    last_name = forms.CharField(label='名 (英字)', max_length=100)
+    first_name = forms.CharField(label='姓 (英字)', max_length=100)
     email = forms.EmailField(label='メール',max_length=250)
     re_email = forms.EmailField(label='メール再入力', max_length=250)
     password = forms.CharField(label='パスワード', max_length=50)
