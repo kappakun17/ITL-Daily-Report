ca155056a1d49c862a7b899beef8bcda721920e5
Ren Murayama <t-rmurayama@microsoft.com>
2022-06-29 02:17:58 +0900

    新しくパスを追加しました。

--- ITL-Daily-Report/ITL_Daily_Report/urls.py
+++ ITL-Daily-Report/ITL_Daily_Report/urls.py
@@ -20,9 +20,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('', include('user.urls'))
+    path('', include('user.urls')),
+    path('', include('profile.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
