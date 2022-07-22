ac7e364b03cfe79bd6558b257be5073bb329908a
Ren Murayama <t-rmurayama@microsoft.com>
2022-07-22 17:03:37 +0900

    デプロイ使用に修正しました。

--- ITL_Daily_Report/urls.py
+++ ITL_Daily_Report/urls.py
@@ -33,7 +33,4 @@ urlpatterns = [
     path('dashboard/', include('schedule.urls')),
     path('dashboard/', include('accountManagement.urls')),
     path('dashboard/', include('dialy.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATICFILES_DIRS)
