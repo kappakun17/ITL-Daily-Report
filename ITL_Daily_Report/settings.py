9a26f5b3ff14fce602bc2f4457d56a81a1d81b08
Ren Murayama <t-rmurayama@microsoft.com>
2022-07-23 02:32:43 +0900

    fixed

--- ITL_Daily_Report/settings.py
+++ ITL_Daily_Report/settings.py
@@ -140,9 +140,9 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static"),])
+#STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static"),])
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
         
 
