b05b25f3edfcb8084944f250254e1f6bb174c384
Ren Murayama <t-rmurayama@microsoft.com>
2022-07-23 01:55:52 +0900

    workflowを修正しました。

--- ITL_Daily_Report/settings.py
+++ ITL_Daily_Report/settings.py
@@ -141,10 +141,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-    STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static"),])
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 
+STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static"),])
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
         
 
