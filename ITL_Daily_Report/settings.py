0444d69e4861319945ab658d49ba1e08347e3465
Ren Murayama <t-rmurayama@microsoft.com>
2022-07-22 17:15:56 +0900

    fixed

--- ITL_Daily_Report/settings.py
+++ ITL_Daily_Report/settings.py
@@ -137,12 +137,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-   ]
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+       ]
  
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
