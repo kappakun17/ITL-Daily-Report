699c308c309b7876a282758729eff4ffec5bae1b
Ren Murayama <t-rmurayama@microsoft.com>
2022-07-23 01:01:22 +0900

    fixed

--- ITL_Daily_Report/urls.py
+++ ITL_Daily_Report/urls.py
@@ -36,5 +36,3 @@ urlpatterns = [
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATICFILES_DIRS)
